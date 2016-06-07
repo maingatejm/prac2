fa88ed8c0ab5cf7a9d8a82ce278b4802be245272
maingate_choi <ttbj123@gmail.com>
2016-06-07 22:41:37 +0900

    Comment Admin 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.humanize.templatetags.humanize import intcomma
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
 	list_display = ['title', 'content_size', 'created_at']
@@ -8,4 +8,9 @@ class PostAdmin(admin.ModelAdmin):
 	def content_size(self, post):
 		return '%s자' % intcomma(len(post.content))
 
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ['post', 'user', 'message']
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Post
 from .forms import CommentForm
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
 	post_list = Post.objects.all()
@@ -11,12 +13,18 @@ def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post,})
 
+
+@login_required
 def comment_new(request, post_pk):
 	if request.method == "POST":
 		form = CommentForm(request.POST)
 		if form.is_valid():
-			comment = form.save()
+			comment = form.save(commit=False)
+			comment.post = get_object_or_404(Post, pk=post_pk)
+			comment.user = request.user
+			comment.save()
 			return redirect('blog:post_detail', post_pk)
 	else:
 		form = CommentForm()
 	return render(request, 'blog/comment_form.html', {'form':form,})
+
