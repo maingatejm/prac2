9ceae313f88dcf8d90b73ad9f075d832df3e96ac
maingate_choi <ttbj123@gmail.com>
2016-06-07 23:14:19 +0900

    comment edit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from .models import Post
+from .models import Post, Comment
 from .forms import CommentForm
 from django.contrib.auth.decorators import login_required
 
@@ -28,3 +28,17 @@ def comment_new(request, post_pk):
 		form = CommentForm()
 	return render(request, 'blog/comment_form.html', {'form':form,})
 
+@login_required
+def comment_edit(request, post_pk, pk):
+	comment = get_object_or_404(Comment, pk = pk)
+	if request.method == 'POST':
+		form = CommentForm(request.POST, instance=comment)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.post = get_object_or_404(Post, pk=post_pk)
+			comment.user = request.user
+			comment.save()
+			return redirect('blog:post_detail', post_pk)
+	else:
+		form = CommentForm(instance=comment)
+	return render(request,'blog/comment_form.html', {'form':form,})
